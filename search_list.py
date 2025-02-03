599edda5d242b8656d3cef4c63895a9300e82468
Zihan-Wang-leeds <wqxh3918@leeds.ac.uk>
2025-02-03 13:41:20 +0000

    Update search_list.py

--- search_list.py
+++ search_list.py
@@ -1,1 +1,16 @@
+from ordered_list import get_ordered_list
 
+def search_ordered_list(lst, target):
+    sorted_lst = get_ordered_list(lst)
+    low, high = 0, len(sorted_lst) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if sorted_lst[mid] == target:
+            return True
+        elif sorted_lst[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return False
